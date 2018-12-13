9423a5ab00454c750883c212612111132075070f
elephantzhai <elephantzhai@gmail.com>
2018-12-14 02:18:59 +0800

    0101change commit

--- oj/leetcode/java/src/problem0101.java
+++ oj/leetcode/java/src/problem0101.java
@@ -33,12 +33,24 @@ public class problem0101 {
 	}
 	
 	public TreeNode genTree(){
-		TreeNode t1 = new TreeNode(1);
-		TreeNode t2 = new TreeNode(2);
+		TreeNode t1 = new TreeNode(2);
+		TreeNode t2 = new TreeNode(3);
 		TreeNode t3 = new TreeNode(3);
 		t1.right = t2;
 		t2.left = t3;
+		
+		TreeNode t4 = new TreeNode(4);
+		TreeNode t5 = new TreeNode(5);
+		t2.left = t4;
+		t2.right = t5;
+		
+		TreeNode t6 = new TreeNode(4);
+		t3.left = null;
+		t3.right = t6;
+		
+		
 		return t1;
 	}
 
 }
+
